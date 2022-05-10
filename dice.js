ced0e4485a39de993529cec15f49efe748954869
Wealthconsole01 <95597057+Wealthconsole01@users.noreply.github.com>
2022-05-10 18:10:11 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,22 @@
+
+function getRandomNumber(){
+    var randomNumber1=Math.floor(Math.random() * 6 + 1);
+    var randomNumber2=Math.floor(Math.random() * 6 + 1);
+}
+
+
+document.querySelector('.img1').setAttribute("src","images/dice"+randomNumber1+".png");
+document.querySelector('.img2').setAttribute("src","images/dice"+randomNumber2+".png");
+
+if (randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player Two Won"
+}
+else if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="Player One Won"
+}
+else {
+    document.querySelector("h1").innerHTML="Both Won"
+}
+
+var button=document.getElementById("btn");
+    button.click()
